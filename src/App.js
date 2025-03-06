f99dea1aef3b5b163398a62e104844dca4c891ee
Matthew Marshall <marsh189@mail.chapman.edu>
2025-03-06 01:02:19 -0800

    change url

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import './index.css';
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:5000');
+const socket = io('https://tic-tac-toe-server-xysi.onrender.com');
 
 const App = () => {
   const [game, setGame] = useState({
